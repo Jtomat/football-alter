6b0caa0b4ce63e333379105b8c62e460114c60e1
Jtomat <stragnik13@gmail.com>
2022-03-01 16:49:11 +0500

    add: example 2

--- src/controllers/team/player-controller.ts
+++ src/controllers/team/player-controller.ts
@@ -3,11 +3,14 @@ import {PlayerRepository} from "../../repositories/player-repository";
 import {Connection} from "typeorm";
 import {Request, Response} from "express";
 import * as _ from "lodash";
+import {TeamRepository} from "../../repositories/team-repository";
 
 // TODO: See example
 export class PlayerController extends BasicController<PlayerRepository> {
+    protected repository2: TeamRepository;
     constructor(connection: Connection) {
         super(connection, PlayerRepository);
+        this.repository2 = connection.getCustomRepository(TeamRepository);
         // init custom method
         this.methodCustom = this.methodCustom.bind(this);
         this.router.get('/a/b', this.methodCustom)
