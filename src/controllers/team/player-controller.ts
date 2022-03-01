35d63d8b92bd2f3569caead00f0f74471d2c3808
Jtomat <stragnik13@gmail.com>
2022-03-01 16:37:20 +0500

    add: example

--- src/controllers/team/player-controller.ts
+++ src/controllers/team/player-controller.ts
@@ -1,9 +1,24 @@
 import BasicController from "../../core/controllers/basic-controller";
 import {PlayerRepository} from "../../repositories/player-repository";
 import {Connection} from "typeorm";
+import {Request, Response} from "express";
+import * as _ from "lodash";
 
+// TODO: See example
 export class PlayerController extends BasicController<PlayerRepository> {
     constructor(connection: Connection) {
         super(connection, PlayerRepository);
+        // init custom method
+        this.methodCustom = this.methodCustom.bind(this);
+        this.router.get('/a/b', this.methodCustom)
+    }
+
+    // override example
+    async methodGet(req: Request, res: Response, next: any): Promise<Response> {
+        return res.json('1');
+    }
+    // custom method example
+    async methodCustom(req: Request, res: Response, next: any): Promise<Response> {
+        return  res.json('Custom')
     }
 }
